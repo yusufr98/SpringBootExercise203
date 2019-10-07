8edd1166a4f50babc42250f4c5e470177741dc87
yusufr98 <yusuf090798@gmail.com>
2019-10-07 14:34:45 -0400

    initial commit

---
+++ src/main/java/com/example/demo/Employee.java
@@ -0,0 +1,40 @@
+package com.example.demo;
+
+public class Employee {
+    private String firstName;
+    private String lastName;
+    private String ssn;
+    private String dob;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+}
---
+++ src/main/java/com/example/demo/SpringBootExercise203Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootExercise203Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootExercise203Application.class, args);
+    }
+
+}
